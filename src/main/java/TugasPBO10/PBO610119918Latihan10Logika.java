31f5645d7a79d3a72814fa9cc487b514ca2d5466
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-10-20 21:57:45 +0700

    PBO6-10119918-Latihan10-Logiika

---
+++ src/main/java/TugasPBO10/PBO610119918Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasPBO10;
+
+/**
+ *
+ * @author andreas
+ */
+public class PBO610119918Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
